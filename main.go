8b01917030ad374e5c3c81c6a011fe20ed80ffed
tsunamicxde <vladislav.cosmos27@gmail.com>
2024-10-03 21:07:14 +0400

    1st commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("YOUR_TELEGRAM_BOT_API_TOKEN")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Авторизация прошла: %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			bot.Send(msg)
+		}
+	}
+}
